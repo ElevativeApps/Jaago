b349fb452c49f3c5a31dbbecec29718910a9be90
devenK16 <elev8iveapps@gmail.com>
2023-12-26 12:32:19 +0530

    Check Boxes functionality

--- app/src/main/java/com/example/jaago/screens/alarm/AddAlarm.kt
+++ app/src/main/java/com/example/jaago/screens/alarm/AddAlarm.kt
@@ -5,13 +5,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.NumberPicker
+import android.widget.TextView
 import android.widget.TimePicker
 import com.example.jaago.R
 import com.google.android.material.button.MaterialButton
 
 class AddAlarm : AppCompatActivity() {
-
+    private lateinit var everyDay: TextView
+    private lateinit var weekDay: TextView
+    private lateinit var weekEnd: TextView
+    private lateinit var cbMonday: CheckBox
+    private lateinit var cbTuesday: CheckBox
+    private lateinit var cbWednesday: CheckBox
+    private lateinit var cbThursday: CheckBox
+    private lateinit var cbFriday: CheckBox
+    private lateinit var cbSaturday: CheckBox
+    private lateinit var cbSunday: CheckBox
+    private var isSelectedEveryDay: Boolean = false
+    private var isSelectedWeekDay: Boolean = false
+    private var isSelectedWeekEnd: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_alarm)
@@ -43,6 +57,84 @@ class AddAlarm : AppCompatActivity() {
             resultIntent.putExtra(SELECTED_TIME, selectedTime)
             setResult(Activity.RESULT_OK, resultIntent)
             finish()
+        }
+
+        init()
+    }
+    private fun init(){
+        everyDay = findViewById(R.id.tv_everyDay)
+        weekDay = findViewById(R.id.tv_weekDay)
+        weekEnd = findViewById(R.id.tv_weekEnd)
+        cbMonday = findViewById(R.id.cb_monday)
+        cbTuesday = findViewById(R.id.cb_tuesday)
+        cbWednesday = findViewById(R.id.cb_wednesday)
+        cbThursday = findViewById(R.id.cb_thursday)
+        cbFriday = findViewById(R.id.cb_friday)
+        cbSaturday = findViewById(R.id.cb_saturday)
+        cbSunday = findViewById(R.id.cb_sunday)
+
+        everyDay.setOnClickListener {
+            markEveryDay()
+        }
+        weekDay.setOnClickListener {
+            markWeekDay()
+        }
+        weekEnd.setOnClickListener {
+            markWeekEnd()
+        }
+    }
+
+    private fun markWeekEnd() {
+        if( isSelectedWeekEnd ){
+            cbSaturday.isChecked = false
+            cbSunday.isChecked = false
+            isSelectedWeekEnd = false
+        } else {
+            cbSaturday.isChecked = true
+            cbSunday.isChecked = true
+            isSelectedWeekEnd = true
+        }
+
+    }
+
+    private fun markWeekDay() {
+        if( isSelectedWeekDay){
+            cbMonday.isChecked = false
+            cbTuesday.isChecked = false
+            cbWednesday.isChecked = false
+            cbThursday.isChecked = false
+            cbFriday.isChecked = false
+            isSelectedWeekDay = false
+        } else {
+            cbMonday.isChecked = true
+            cbTuesday.isChecked = true
+            cbWednesday.isChecked = true
+            cbThursday.isChecked = true
+            cbFriday.isChecked = true
+            isSelectedWeekDay = true
+        }
+
+    }
+
+    private fun markEveryDay() {
+        if( isSelectedEveryDay ){
+            cbMonday.isChecked = false
+            cbTuesday.isChecked = false
+            cbWednesday.isChecked = false
+            cbThursday.isChecked = false
+            cbFriday.isChecked = false
+            cbSaturday.isChecked = false
+            cbSunday.isChecked = false
+            isSelectedEveryDay = false
+        } else {
+            cbMonday.isChecked = true
+            cbTuesday.isChecked = true
+            cbWednesday.isChecked = true
+            cbThursday.isChecked = true
+            cbFriday.isChecked = true
+            cbSaturday.isChecked = true
+            cbSunday.isChecked = true
+            isSelectedEveryDay = true
         }
     }
 
